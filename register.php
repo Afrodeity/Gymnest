edb168b72efbb37a110c1b83272ea58b79f89c67
Afrodeity <noahschick@gmx.de>
2016-04-10 19:09:39 +0200

    registration changes

--- register.php
+++ register.php
@@ -4,10 +4,10 @@ include_once 'config.php';
 
 $error_msg = "";
 
-if (isset($_POST['username'], $_POST['p'])) {
+if (isset($_POST['username'], $_POST['password'])) {
 	//Input Sanitizarion
 	$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-	$password = filter_input(INPUT_POST, 'p', FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 	
 	$stmt = "SELECT userID FROM users WHERE username = ? LIMIT 1";
 	$stmt = $mysqli->prepare($stmt);
@@ -27,8 +27,8 @@ if (isset($_POST['username'], $_POST['p'])) {
 	if(empty($error_msg)) {
 		$password = password_hash($password, PASSWORD_DEFAULT);
 		//Insert new user to database
-		if($insert_stmt = $mysqli->prepare("INSERT INTO users (username, password, salt) VALUES (?, ?, ?)")) {
-			$insert_stmt->bind_param('sss', $username, $password, $salt);
+		if($insert_stmt = $mysqli->prepare("INSERT INTO users (username, password) VALUES (?, ?, ?)")) {
+			$insert_stmt->bind_param('ss', $username, $password);
 			if(! $insert_stmt->execute()) {
 				//$error_msg .= "<p class="error">Error creating user. Please try again.</p>";
 				header('Location: ./error.php?err=Error creating user. Please try again.');
@@ -36,7 +36,7 @@ if (isset($_POST['username'], $_POST['p'])) {
 			}
 		}
 	//Redirect to dashboard upon success
-	header('Location: ./registered.php');
+	header('Location: ./portal.html');
 	exit;
 	}
 }
